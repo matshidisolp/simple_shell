39d7ead4215b21ac080950493e985f1bbd384d43
okibe onmeje <okibeogomola@gmail.com>
2023-08-22 08:18:59 -0700

    feat program now handles special inputs
    - strings starting with the following characters are handled
    * \t\s\a\r
    * string containing just a newline character are handled

--- main.c
+++ main.c
@@ -10,12 +10,17 @@ int main(void)
 	char *buff, *delim;
 	char **tokens;
 
-	delim = " \n";
+	delim = " \n\t\a\r";
 	status = 0;
 	do {
 		prompt();
 		status = readline(&buff, &n);
 		tokens = get_token(buff, delim);
+		if (*buff == '\n' || *tokens == NULL)
+		{
+			free(tokens);
+			continue;
+		}
 		handle_exit(tokens);
 		lunch(tokens);
 		free(tokens);
